8f43dc4c679c780019ee2bb0032708746ab1eeea
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-01-27 17:48:19 +0530

    27th January 2025

---
+++ MaximumAverageSubarray1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double maxSum = 0;
+
+        for(int i = 0; i < k; i++){
+            maxSum = maxSum + nums[i];
+        }
+
+        double currentSum = maxSum;
+
+        for(int i = k; i < nums.length; i++){
+            currentSum = currentSum + nums[i] - nums[i-k];
+            maxSum = Math.max(maxSum,currentSum);
+        }
+        return maxSum/k;
+    }
+}
